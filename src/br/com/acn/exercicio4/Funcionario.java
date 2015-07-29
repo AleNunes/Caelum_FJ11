d9a82266bdf1d18267db2fa9da700e4f7c575e87
Alexander C Nunes <alexandercn@hotmail.com>
2015-07-28 21:52:43 -0300

    Capitulo 4

---
+++ src/br/com/acn/exercicio4/Funcionario.java
@@ -0,0 +1,69 @@
+package br.com.acn.exercicio4;
+
+public class Funcionario {
+
+	private String nome;
+	private String depto;
+	private double salario;
+	private String dataEntrada;
+	private String rg;
+	private boolean estaNaEmpresa;
+
+	// GETs & SETs
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDepto() {
+		return depto;
+	}
+
+	public void setDepto(String depto) {
+		this.depto = depto;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public String getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(String dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public boolean isEstaNaEmpresa() {
+		return estaNaEmpresa;
+	}
+
+	public void setEstaNaEmpresa(boolean estaNaEmpresa) {
+		this.estaNaEmpresa = estaNaEmpresa;
+	}
+	// --
+
+	// Manipulacao e Visualizacao de Salario
+	void recebeAumento(double aumento) {
+		this.salario += aumento;
+	}
+
+	double calculaGanhoAnual() {
+		return this.getSalario() * 12;
+	}
+	// --
+
+}
